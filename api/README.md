acc83b503cd333f56820d300010b2c24920c41f3
Steven Rogers <steven.rogers@thomsonreuters.com>
2017-11-28 22:11:15 -0600

    Added a hook to start the REST server.  Added some cleanup on the MD file.

--- api/README.md
+++ api/README.md
@@ -1,19 +1,26 @@
 # ngAngular4 API
+
 A simple API server.
 
 ## Adding new endpoints
-1. Within index.js, you can add more routes:
-```
+
+ Within index.js, you can add more routes:
+
+```javascript
 server.get("users/", ...);
 server.post("users/", ...);
 ```
-2. Within routes.js, you add the functionality for retrieving and sending the data from the database and connecting that functionality to the route in index.js.  Example:
-```
+
+ Within routes.js, you add the functionality for retrieving and sending the data from the database and connecting that functionality to the route in index.js.  Example:
+
+```javascript
 server.get("users/", route.getAllUsers);
 ```
 
 ## Running the server
-1. In git bash (or any terminal), run the following command:
-```
+
+In git bash (or any terminal), run the following command:
+
+```console
 npm start
 ```
--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,21 @@
+
 const server = require('restify').createServer();
-const route = require('../src/routes');
+const color = require('node-colorify');
+
+const routes = require('../src/routes');
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   next();
 });
 
 // routes
-server.get("users/", route.getAllUsers);
+server.get('users/', routes.getAllUsers);
 
-server.listen(3050, '127.0.0.1', function () {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(3050, '127.0.0.1', () => {
+  console.log(
+    color.colorItSync(`REST server running at ${server.url}`, { 'fColor': 'green' })
+  );
 });
