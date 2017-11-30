aaf81d28510380d4c526fe9954d63ec41ca06d26
Steven Rogers <steven.rogers@thomsonreuters.com>
2017-11-30 15:40:33 -0600

    Removed the mock data json and added mongo db implementation to the routes.

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,10 +1,22 @@
-const mockData = require('../src/mockdata.json');
+const mongo = require('mongojs');
+const color = require('node-colorify');
+
+const db = mongo('mongodb://lunchlearn:Reuters909!@ds119446.mlab.com:19446/lunchlearn', ['Users']);
 
 const svc = {};
 
 svc.getAllUsers = (req, res, next) => {
-  res.json(200, mockData.users);
-  next();
+  db.Users.find((err, docs) => {
+    if (err) {
+      console.error(
+        color.colorItSync('An error occurred while attempting to query Users.', { 'fColor': 'red' })
+      );
+      next();
+    }
+
+    res.json(200, docs);
+    next();
+  });
 };
 
 module.exports = svc;
